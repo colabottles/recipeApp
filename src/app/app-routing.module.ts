afc136a309b8341b7dd724f368ac83ef4da87a8f
Todd Libby <toddlibby@protonmail.com>
2026-05-03 12:57:27 -0700

    fix: restore routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RecipeListComponent },
+  { path: 'add', component: RecipeFormComponent },
+  { path: 'recipe/:id', component: RecipeDetailComponent },
+  { path: 'recipe/:id/edit', component: RecipeEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
