b74adf3088df45a620a46665ab840c4110850b19
Todd Libby <toddlibby@protonmail.com>
2026-05-04 18:13:14 -0700

    fix: update environments.ts, rename to Mise

---
+++ src/app/environments/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://rspcbwvgumioahxfazlb.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzcGNid3ZndW1pb2FoeGZhemxiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Nzc4MjgxNiwiZXhwIjoyMDkzMzU4ODE2fQ.qgDresDL6Us_gfkML1mX6_N4BUMMYXsCyomsa9JtAdg'
+};
